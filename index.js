8b8423002d4d5520f84c5d07690ce61ff5111d03
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2023-11-14 06:46:43 +0600

    server ready

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient } = require("mongodb");
+
 const app = express()
 const port = process.env.PORT || 5000;
 
 
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  `mongodb+srv://${username}:${password}@${cluster}/?authSource=${authSource}&authMechanism=${authMechanism}`;
 
 
 app.get('/', (req, res) => {
